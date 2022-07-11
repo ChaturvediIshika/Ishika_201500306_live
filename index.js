187091b17d6273133b94cbec1e6deaeace698b0d
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2022-07-11 18:55:04 +0530

    commit

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("name").innerHTML="Ishika Chaturvedi";
+document.getElementById("roll-no").innerHTML="201500306";
+document.getElementById("university").innerHTML="GLA University, Mathura";
+
+var date=new Date();
+
+document.getElementById("date").innerHTML=date.toDateString();
